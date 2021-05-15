d15d8373bda0144703c2fa34bc200e92fa047450
Crowdbotics <team@crowdbotics.com>
2021-05-15 15:25:59 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/scoldrem_city_26722/MainActivity.java
@@ -0,0 +1,15 @@
+package com.scoldrem_city_26722;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "scoldrem_city_26722";
+  }
+}
